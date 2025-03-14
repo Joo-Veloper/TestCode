eac8af7a97030d58b08cf4961620e733570d5a77
Joo-Veloper <psjoo3515@gmail.com>
2025-03-14 18:07:26 +0900

    test: Inventory quantity verification and deduction function test added

--- src/main/java/io/cafekiosk/spring/api/order/service/OrderService.java
+++ src/main/java/io/cafekiosk/spring/api/order/service/OrderService.java
@@ -5,14 +5,19 @@ import io.cafekiosk.spring.api.order.dto.OrderResponseDto;
 import io.cafekiosk.spring.domain.order.entity.Order;
 import io.cafekiosk.spring.domain.order.repository.OrderRepository;
 import io.cafekiosk.spring.domain.product.entity.Product;
+import io.cafekiosk.spring.domain.product.entity.ProductType;
 import io.cafekiosk.spring.domain.product.repository.ProductRepository;
+import io.cafekiosk.spring.domain.stock.entity.Stock;
+import io.cafekiosk.spring.domain.stock.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -20,10 +25,36 @@ public class OrderService {
 
     private final ProductRepository productRepository;
     private final OrderRepository orderRepository;
+    private final StockRepository stockRepository;
 
     public OrderResponseDto createOrder(OrderCreateRequestDto requestDto, LocalDateTime registeredDateTime) {
         List<String> productNumbers = requestDto.getProductNumbers();
         List<Product> products = findProductsBy(productNumbers);
+
+        // 재고 차감 체크가 필요한 상품 filter
+        List<String> stockProductNumbers = products.stream()
+                .filter(product -> ProductType.containsStockType(product.getType()))
+                .map(Product::getProductNumber)
+                .toList();
+
+        // 재고 엔티티 조회
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
+        Map<String, Stock> stockMap = stocks.stream()
+                .collect(Collectors.toMap(Stock::getProductNumber, s -> s));
+
+        // 상품별 counting
+        Map<String, Long> productCountingMap = stockProductNumbers.stream()
+                .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
+
+        // 재고 차감 시도
+        for (String stockProductNumber : new HashSet<>(stockProductNumbers)) {
+            Stock stock = stockMap.get(stockProductNumber);
+            int quantity = productCountingMap.get(stockProductNumber).intValue();
+            if (stock.isQuantityLessThan(quantity)) {
+                throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
+            }
+            stock.deductQuantity(quantity);
+        }
 
         Order order = Order.create(products, registeredDateTime);
         Order savedOrder = orderRepository.save(order);
--- src/test/java/io/cafekiosk/spring/api/order/service/OrderServiceTest.java
+++ src/test/java/io/cafekiosk/spring/api/order/service/OrderServiceTest.java
@@ -51,52 +51,6 @@ class OrderServiceTest {
         orderRepository.deleteAllInBatch();
     }*/
 
-    @DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트르 받아 주문을 생성한다.")
-    @Test
-    void createOrderWithStock() {
-        // given
-        LocalDateTime registeredDateTime = LocalDateTime.now();
-
-        Product product1 = createProduct(BOTTLE, "001", 1000);
-        Product product2 = createProduct(BAKERY, "002", 5000);
-        Product product3 = createProduct(HANDMADE, "003", 10000);
-
-        productRepository.saveAll(List.of(product1, product2, product3));
-
-        Stock stock1= Stock.create("001", 2);
-        Stock stock2= Stock.create("002", 2);
-        stockRepository.saveAll(List.of(stock1, stock2));
-
-        OrderCreateRequestDto requestDto = OrderCreateRequestDto.builder()
-                .productNumbers(List.of("001","001", "002", "003"))
-                .build();
-
-        // when
-        OrderResponseDto orderResponseDto = orderService.createOrder(requestDto, registeredDateTime);
-
-        // then
-        assertThat(orderResponseDto.getId()).isNotNull();
-        assertThat(orderResponseDto)
-                .extracting("registeredDateTime", "totalPrice")
-                .contains(registeredDateTime, 17000);
-        assertThat(orderResponseDto.getProducts()).hasSize(4)
-                .extracting("productNumber", "price")
-                .containsExactlyInAnyOrder(
-                        tuple("001", 1000),
-                        tuple("001", 1000),
-                        tuple("002", 5000),
-                        tuple("003", 10000)
-                );
-
-        List<Stock> stocks = stockRepository.findAll();
-        assertThat(stocks).hasSize(2)
-                .extracting("productNumber", "quantity")
-                .containsExactlyInAnyOrder(
-                        tuple("001", 0),
-                        tuple("002", 1)
-                );
-    }
-
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     @Test
     void createOrder() {
@@ -158,6 +112,52 @@ class OrderServiceTest {
                 .containsExactlyInAnyOrder(
                         tuple("001", 1000),
                         tuple("001", 1000)
+                );
+    }
+
+    @DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트르 받아 주문을 생성한다.")
+    @Test
+    void createOrderWithStock() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 5000);
+        Product product3 = createProduct(HANDMADE, "003", 10000);
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1= Stock.create("001", 2);
+        Stock stock2= Stock.create("002", 2);
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequestDto requestDto = OrderCreateRequestDto.builder()
+                .productNumbers(List.of("001","001", "002", "003"))
+                .build();
+
+        // when
+        OrderResponseDto orderResponseDto = orderService.createOrder(requestDto, registeredDateTime);
+
+        // then
+        assertThat(orderResponseDto.getId()).isNotNull();
+        assertThat(orderResponseDto)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 17000);
+        assertThat(orderResponseDto.getProducts()).hasSize(4)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("001", 1000),
+                        tuple("002", 5000),
+                        tuple("003", 10000)
+                );
+
+        List<Stock> stocks = stockRepository.findAll();
+        assertThat(stocks).hasSize(2)
+                .extracting("productNumber", "quantity")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 0),
+                        tuple("002", 1)
                 );
     }
 
