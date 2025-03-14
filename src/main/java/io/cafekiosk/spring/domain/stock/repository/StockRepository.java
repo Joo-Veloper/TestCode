20de1f693a9d0e5b32f0214119daca1f371cbf7d
Joo-Veloper <psjoo3515@gmail.com>
2025-03-14 17:20:44 +0900

    test: Added inventory check function test with product number list

--- src/main/java/io/cafekiosk/spring/domain/stock/repository/StockRepository.java
+++ src/main/java/io/cafekiosk/spring/domain/stock/repository/StockRepository.java
@@ -4,7 +4,10 @@ import io.cafekiosk.spring.domain.stock.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
+    List<Stock> findAllByProductNumberIn(List<String> productNumbers);
 }
