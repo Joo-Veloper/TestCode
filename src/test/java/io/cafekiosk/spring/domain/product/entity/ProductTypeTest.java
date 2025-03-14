580efe09b351b47650423eee4a36f4c675f05fc9
Joo-Veloper <psjoo3515@gmail.com>
2025-03-14 16:03:52 +0900

    test: Add a test to determine if the product type is related to inventory

---
+++ src/test/java/io/cafekiosk/spring/domain/product/entity/ProductTypeTest.java
@@ -0,0 +1,33 @@
+package io.cafekiosk.spring.domain.product.entity;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ProductTypeTest {
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @Test
+    void containsStockType() {
+        // given
+        ProductType givenType = ProductType.HANDMADE;
+        // when
+        boolean result = ProductType.containsStockType(givenType);
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+    @Test
+    void containsStockType2() {
+        // given
+        ProductType givenType = ProductType.BAKERY;
+        // when
+        boolean result = ProductType.containsStockType(givenType);
+        // then
+        assertThat(result).isTrue();
+    }
+}
