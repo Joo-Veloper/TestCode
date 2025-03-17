2c4a2dc9b44b093ff2256f62d3f70f211b8d7d90
Joo-Veloper <psjoo3515@gmail.com>
2025-03-17 16:35:50 +0900

    refactor: Improve product generation logic and readability

--- src/main/java/io/cafekiosk/spring/api/product/service/ProductService.java
+++ src/main/java/io/cafekiosk/spring/api/product/service/ProductService.java
@@ -3,8 +3,6 @@ package io.cafekiosk.spring.api.product.service;
 import io.cafekiosk.spring.api.product.dto.request.ProductCreateRequestDto;
 import io.cafekiosk.spring.api.product.dto.response.ProductResponseDto;
 import io.cafekiosk.spring.domain.product.entity.Product;
-import io.cafekiosk.spring.domain.product.entity.ProductSellingStatus;
-import io.cafekiosk.spring.domain.product.entity.ProductType;
 import io.cafekiosk.spring.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,8 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static io.cafekiosk.spring.domain.product.entity.ProductSellingStatus.*;
-import static io.cafekiosk.spring.domain.product.entity.ProductType.*;
+import static io.cafekiosk.spring.domain.product.entity.ProductSellingStatus.forDisplay;
 
 @Service
 @RequiredArgsConstructor
@@ -21,15 +18,25 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     public ProductResponseDto createProduct(ProductCreateRequestDto requestDto) {
-        String latestProductNumber = productRepository.findLatestProductNumber();
+        String nextProductNumber = createNextProductNumber();
 
         return ProductResponseDto.builder()
-                .productNumber("002")
-                .type(HANDMADE)
-                .sellingStatus(SELLING)
-                .name("카푸치노")
-                .price(5000)
+                .productNumber(nextProductNumber)
+                .type(requestDto.getType())
+                .sellingStatus(requestDto.getSellingStatus())
+                .name(requestDto.getName())
+                .price(requestDto.getPrice())
                 .build();
+    }
+
+    private String createNextProductNumber(){
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        // %03d = 9 -> 009, 10 -> 010
+        return String.format("%03d", nextProductNumberInt);
     }
 
 
