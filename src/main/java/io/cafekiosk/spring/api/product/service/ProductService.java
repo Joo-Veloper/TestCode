b6b41e8f1c6050309534bf9830f1a35519a5f5d9
Joo-Veloper <psjoo3515@gmail.com>
2025-03-17 17:12:16 +0900

    feat: Add @Transactional (readOnly = true) to optimize performance

--- src/main/java/io/cafekiosk/spring/api/product/service/ProductService.java
+++ src/main/java/io/cafekiosk/spring/api/product/service/ProductService.java
@@ -6,17 +6,29 @@ import io.cafekiosk.spring.domain.product.entity.Product;
 import io.cafekiosk.spring.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static io.cafekiosk.spring.domain.product.entity.ProductSellingStatus.forDisplay;
 
+/**
+ * readOnly = true : 읽기 전용
+ * CRUD 에서 CUD 동작 X / Only Read
+ * JPA : CUD 스냅샷 저장, 변경감지 안해도 되는 이점 생김( 성능 향상 )
+ *
+ * CQRS : Command 와 query 를 분리하자 ( 책임을 분리해서 서로 연관이 없게 하자 )
+ *
+ * @Transactional(readOnly = true) 전체 달고 메서드별 CUD에 @Transaction을 다시 달아주는방법 추천
+ */
+@Transactional(readOnly = true)
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
 
+    @Transactional
     public ProductResponseDto createProduct(ProductCreateRequestDto requestDto) {
         String nextProductNumber = createNextProductNumber();
 
@@ -26,7 +38,15 @@ public class ProductService {
         return ProductResponseDto.of(savedProduct);
     }
 
-    private String createNextProductNumber(){
+    public List<ProductResponseDto> getSellingProducts() {
+        List<Product> products = productRepository.findAllBySellingStatusIn(forDisplay());
+
+        return products.stream()
+                .map(ProductResponseDto::of)
+                .collect(Collectors.toList());
+    }
+
+    private String createNextProductNumber() {
         String latestProductNumber = productRepository.findLatestProductNumber();
 
         if (latestProductNumber == null) {
@@ -40,12 +60,4 @@ public class ProductService {
         return String.format("%03d", nextProductNumberInt);
     }
 
-
-    public List<ProductResponseDto> getSellingProducts() {
-        List<Product> products = productRepository.findAllBySellingStatusIn(forDisplay());
-
-        return products.stream()
-                .map(ProductResponseDto::of)
-                .collect(Collectors.toList());
-    }
 }
