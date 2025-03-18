afead65ece166ad14b80c6abb00a291cf62c856b
Joo-Veloper <psjoo3515@gmail.com>
2025-03-18 14:12:35 +0900

    feat: Add Validation to Product Creation Request DTO

--- src/main/java/io/cafekiosk/spring/api/product/controller/ProductController.java
+++ src/main/java/io/cafekiosk/spring/api/product/controller/ProductController.java
@@ -3,12 +3,12 @@ package io.cafekiosk.spring.api.product.controller;
 import io.cafekiosk.spring.api.product.dto.request.ProductCreateRequestDto;
 import io.cafekiosk.spring.api.product.dto.response.ProductResponseDto;
 import io.cafekiosk.spring.api.product.service.ProductService;
+import io.cafekiosk.spring.global.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,7 +19,7 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/new")
-    public void createProduct(ProductCreateRequestDto requestDto) {
+    public void createProduct(@Valid @RequestBody ProductCreateRequestDto requestDto) {
         productService.createProduct(requestDto);
     }
 
--- src/main/java/io/cafekiosk/spring/api/product/dto/request/ProductCreateRequestDto.java
+++ src/main/java/io/cafekiosk/spring/api/product/dto/request/ProductCreateRequestDto.java
@@ -5,13 +5,25 @@ import io.cafekiosk.spring.domain.product.entity.ProductSellingStatus;
 import io.cafekiosk.spring.domain.product.entity.ProductType;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Getter
+@NoArgsConstructor
 public class ProductCreateRequestDto {
 
+    @NotNull
     private ProductType type;
+
+    @NotNull
     private ProductSellingStatus sellingStatus;
+
+    @NotNull
     private String name;
+
+    @Positive
     private int price;
 
     @Builder
