3d9f6834bde526c3d2d37fac10296effea7a64b8
Joo-Veloper <psjoo3515@gmail.com>
2025-03-22 16:14:52 +0900

    test: Add Mail Transfer Service Unit Test Refactoring

--- src/test/java/io/cafekiosk/spring/api/mail/service/MailServiceTest.java
+++ src/test/java/io/cafekiosk/spring/api/mail/service/MailServiceTest.java
@@ -5,26 +5,31 @@ import io.cafekiosk.spring.domain.hisotry.entity.mail.MailSendHistory;
 import io.cafekiosk.spring.domain.hisotry.repository.MailSendHistoryRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class MailServiceTest {
+    @Mock
+    private MailSendClient mailSendClient;
+
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @InjectMocks
+    private MailService mailService;
 
     @DisplayName("메일 전송 테스트")
     @Test
     void sendMail() {
-        // given
-        MailSendClient mailSendClient = Mockito.mock(MailSendClient.class);
-        MailSendHistoryRepository mailSendHistoryRepository = Mockito.mock(MailSendHistoryRepository.class);
-
-        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
-
-        // stubbing
+        // given & stubbing
         when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
                 .thenReturn(true);
 
@@ -33,6 +38,6 @@ class MailServiceTest {
 
         // then
         assertThat(result).isTrue();
-        Mockito.verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+        verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
     }
 }
