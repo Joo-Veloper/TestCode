65770b85547049c4b2a5ffd924c0cb603d2f6c79
Joo-Veloper <psjoo3515@gmail.com>
2025-04-22 18:08:28 +0900

    doc: add OpenAPI definition for Swagger UI

--- src/main/java/io/cafekiosk/spring/CafekioskApplication.java
+++ src/main/java/io/cafekiosk/spring/CafekioskApplication.java
@@ -1,9 +1,16 @@
 package io.cafekiosk.spring;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Toy project",
+        version = "v1",
+        description = "A toy project used for adding and practicing test code"
+))
 public class CafekioskApplication {
 
     public static void main(String[] args) {
