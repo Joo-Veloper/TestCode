cca28eb17b388f3cedf268ecfa2034c93503130e
Joo-Veloper <psjoo3515@gmail.com>
2025-04-24 17:54:57 +0900

    test: add tests for creating, retrieving, and updating posts in PostService

---
+++ src/main/resources/sql/post-service-test-data.sql
@@ -0,0 +1,6 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'joo@test.com', 'tester', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'joo1@test.com', 'tester1', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
+insert into `posts` (`id`, `content`, `created_at`, `modified_at`, `user_id`)
+values (1, 'helloWorld', 1678530673958, 0, 1);
