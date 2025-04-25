3c6131b4006ea686cdc2248ca399f8ced750615a
Joo-Veloper <psjoo3515@gmail.com>
2025-04-25 17:27:59 +0900

    test: add integration tests for user API (read, verify, update)

---
+++ src/main/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,4 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'joo@test.com', 'tester', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'joo1@test.com', 'tester1', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
