93bfa7d5d337240d93491eeb6835c926d1ace7b3
Joo-Veloper <psjoo3515@gmail.com>
2025-04-26 17:06:42 +0900

    test: Add test for creating a post

---
+++ src/test/java/io/cafekiosk/spring/api/post/controller/PostCreateControllerTest.java
@@ -0,0 +1,52 @@
+package io.cafekiosk.spring.api.post.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.cafekiosk.spring.api.post.dto.PostCreateDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@SqlGroup({
+        @Sql(value = "/sql/post-create-controller-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+class PostCreateControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void 사용자는_회원_가입을_할_수_있고_회원가입된_사용자는_PENDING_상태이다() throws Exception {
+        //given
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .writerId(1)
+                .content("helloworld")
+                .build();
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(postCreateDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.content").value("helloworld"))
+                .andExpect(jsonPath("$.writer.id").isNumber())
+                .andExpect(jsonPath("$.writer.email").value("joo@test.com"))
+                .andExpect(jsonPath("$.writer.nickname").value("tester"));
+
+    }
+}
